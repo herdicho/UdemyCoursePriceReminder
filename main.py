a8c492c4e0ce6f52f4b3c40c4aef9363c5cb5621
Farras Herdicho <you@example.com>
2020-11-19 20:51:15 +0700

    add credential file & automate login to udemy webpage

--- main.py
+++ main.py
@@ -1,15 +1,67 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Credentials
-sender = "reminderemail09@gmail.com"
-senderPass = "reminder09*"
-receiver = "reminderemail09@gmail.com"
+def readCredentials():
+    emailUdemy = passwordUdemy = ""
+    with open("credentials.txt", "r") as f:
+        file = f.readlines()
+        emailUdemy = file[0].strip()
+        passwordUdemy = file[1].strip()
+        emailSender = file[2].strip()
+        passwordSender = file[3].strip()
+        emailReceiver = file[4].strip()
+
+    return emailUdemy, passwordUdemy, emailSender, passwordSender, emailReceiver
+
+def getElement(selector, elementSelector, waitTime):
+   try:
+      element = WebDriverWait(driver, waitTime).until(
+         EC.presence_of_element_located((selector, elementSelector))
+      )
+   finally:
+      return element
+
+emailUdemy, passwordUdemy, emailSender, passwordSender, emailReceiver = readCredentials()
+
+credential = {
+   "EmailUdemy"   : emailUdemy,
+   "PasswordUdemy" : passwordUdemy,
+   "EmailSender" : emailSender, 
+   "PasswordSender" : passwordSender,
+   "EmailReceiver" : emailReceiver,
+}
+
+element = {
+   "LoginButtonHomePage" : "/html/body/div[1]/div[1]/div[2]/div[5]",
+   "EmailField" : "email--1",
+   "PasswordField" : "id_password",
+   "LoginButtonLoginPage" : "submit-id-submit"
+}
+
+# initiate browser & open udemy webpage
+driver = webdriver.Chrome()
+driver.get("http://udemy.com")
+
+# login
+loginButtonHomePageElement = getElement(By.XPATH, element.get("LoginButtonHomePage"), 10)
+loginButtonHomePageElement.click()
+emailFieldElement = getElement(By.ID, element.get("EmailField"), 10)
+emailFieldElement.send_keys(credential.get("EmailUdemy"))
+passwordFieldElement = getElement(By.ID, element.get("PasswordField"), 10)
+passwordFieldElement.send_keys(credential.get("PasswordUdemy"))
+loginButtonLoginPageElement = getElement(By.ID, element.get("LoginButtonLoginPage"), 10)
+loginButtonLoginPageElement.click()
+
+driver.close()
 
 msg = MIMEMultipart()
-msg['From'] = sender
-msg['To'] = receiver
+msg['From'] = credential.get("EmailSender")
+msg['To'] = credential.get("EmailReceiver")
 msg['Subject'] = "JUDUL PESAN"
  
 body = '''\
@@ -25,8 +77,8 @@ print("Try to send email")
 try:
    server = smtplib.SMTP('smtp.gmail.com', 587)
    server.starttls()
-   server.login(sender, senderPass)
-   server.sendmail(sender, receiver, text)
+   server.login(credential.get("EmailSender"), credential.get("PasswordSender"))
+   server.sendmail(credential.get("EmailSender"), credential.get("EmailReceiver"), text)
    server.quit()
    print("Success send email")
 except:
