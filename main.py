2c18c934e82c8dae1ce3d27e7157d37730562a7a
Farras Herdicho <you@example.com>
2020-11-23 11:26:03 +0700

    add function to send email notif when course or language not found on udemy webpage

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import sys
 
 def readCredentials():
     emailUdemy = passwordUdemy = ""
@@ -50,7 +51,7 @@ element = {
 
 data = {
    "Course" : "Go: The Complete Developer's Guide (Golang)",
-   "MaxPrice" : 150000,
+   "MaxPrice" : 120000,
    "CoursePrice" : 0, # next will be overwritten with the course price
    "CourseLanguage" : "English",
 }
@@ -85,6 +86,34 @@ def sendEmail():
    except:
       print("Fail send email")
 
+def sendErrorEmail(error):
+   subjectContent = "(Language Not Found)" if error == "languageNotFound" else "(Course Not Found)"
+   bodyContent = "Language Not Found" if error == "languageNotFound" else "Course Not Found"
+
+   msg = MIMEMultipart()
+   msg['From'] = credential.get("EmailSender")
+   msg['To'] = credential.get("EmailReceiver")
+   msg['Subject'] = "UDEMY COURSE PRICE REMINDER ERROR "+ subjectContent + ""
+ 
+   body = '''\
+   ''' + bodyContent + '''   
+   Please check the name of course or language
+   '''
+
+   msg.attach(MIMEText(body, 'plain'))
+   text = msg.as_string()
+
+   print("Try to send error email")
+   try:
+      server = smtplib.SMTP('smtp.gmail.com', 587)
+      server.starttls()
+      server.login(credential.get("EmailSender"), credential.get("PasswordSender"))
+      server.sendmail(credential.get("EmailSender"), credential.get("EmailReceiver"), text)
+      server.quit()
+      print("Success send error email")
+   except:
+      print("Fail send error email")
+
 def loginUdemyWebpage():
    loginButtonHomePageElement = getElement(By.XPATH, element.get("LoginButtonHomePage"), 30)
    time.sleep(7)
@@ -105,6 +134,8 @@ def searchCourse():
    time.sleep(5)
 
 def filterCourseLanguage():
+   isLanguageFound = False
+
    # filter course by language of the course (if just input course name, sometimes we cant find the desired course)
    showMoreLanguage = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/div[2]/div/div[2]/div/div[1]/form/div/div[2]/div[2]/div/div/button/span/span[1]")
    showMoreLanguage.click()
@@ -114,22 +145,30 @@ def filterCourseLanguage():
    langIndeks = -1
    for i in listLanguage:
       if (data.get("CourseLanguage").lower() in i.text.lower()):
+         isLanguageFound = True
          langIndeks = indeks
          break
 
       indeks += 1
 
-   languageFilter = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/div[2]/div[1]/div[2]/div/div[1]/form/div/div[2]/div[2]/div/div/div/div/fieldset/label["+str(langIndeks+1)+"]")
-   languageFilter.click()
-
+   if (isLanguageFound):
+      languageFilter = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/div[2]/div[1]/div[2]/div/div[1]/form/div/div[2]/div[2]/div/div/div/div/fieldset/label["+str(langIndeks+1)+"]")
+      languageFilter.click()
+   else:
+      sendErrorEmail("languageNotFound")
+      sys.exit()
+      
    time.sleep(3)
 
 def getCoursePrice():
+   isCourseFound = False
+   
    # search for first top 15 course according to the desired course and get course price then send email if price less than desired price
    for i in range (1, 15):
       try:
          title = getElement(By.XPATH, "/html/body/div[2]/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/div[2]/div["+str(i)+"]/a/div/div[2]/div[1]", 5).text
          if (title == data.get("Course")):
+            isCourseFound = True
             price = getElement(By.XPATH, "/html/body/div[2]/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/div[2]/div["+str(i)+"]/a/div/div[2]/div[5]/div[1]/span[2]/span", 5).text
             priceInteger = int(price.replace("Rp", "").replace(".","").replace(",",""))
             if (priceInteger < data.get("MaxPrice")):
@@ -138,6 +177,10 @@ def getCoursePrice():
             break
       except:
          continue
+   
+   if (not isCourseFound):
+      sendErrorEmail("courseNotFound")
+      sys.exit()
 
 def getUdemyCoursePrice():
    # open udemy webpage
